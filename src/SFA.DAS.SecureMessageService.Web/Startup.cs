9ec0c646f89929a6076264833da218fa2b386b12
Craig Gumbley <craiggumbley@gmail.com>
2019-03-09 15:04:24 +0000

    Cleanup app startup

--- src/SFA.DAS.SecureMessageService.Web/Startup.cs
+++ src/SFA.DAS.SecureMessageService.Web/Startup.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.DataProtection;
-using Microsoft.AspNetCore.DataProtection.StackExchangeRedis;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,14 +13,6 @@ using SFA.DAS.SecureMessageService.Core.Services;
 using SFA.DAS.SecureMessageService.Infrastructure.Repositories;
 using StackExchange.Redis;
 using Microsoft.Extensions.Logging;
-using SFA.DAS.SecureMessageService.Infrastructure;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication;
-using System.Security.Claims;
-using Microsoft.AspNetCore.Authentication.OAuth;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using Newtonsoft.Json.Linq;
 using Microsoft.AspNetCore.Authorization;
 
 namespace SFA.DAS.SecureMessageService.Web
@@ -81,7 +68,6 @@ namespace SFA.DAS.SecureMessageService.Web
 
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
@@ -93,78 +79,7 @@ namespace SFA.DAS.SecureMessageService.Web
                 options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
             });
 
-            services.AddAuthentication(options =>
-                {
-                    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = "GitHub";
-                })
-                .AddCookie(options =>
-                    {
-                        options.Cookie.Name = "sms-auth";
-                        options.AccessDeniedPath = "/AccessDenied/";
-                        options.ReturnUrlParameter = "/";
-                        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-                        // options.Cookie.SameSite = SameSiteMode.Strict;
-                        options.SlidingExpiration = true;
-                        options.Events.OnSigningIn = (context) =>
-                        {
-                            context.CookieOptions.Expires = DateTimeOffset.UtcNow.AddMinutes(30);
-                            return Task.CompletedTask;
-                        };
-                    }
-                )
-                .AddOAuth("GitHub", options =>
-                {
-                    options.ClientId = Configuration["GitHub:ClientId"];
-                    options.ClientSecret = Configuration["GitHub:ClientSecret"];
-                    options.CallbackPath = new PathString("/signin-github");
-                    options.Scope.Add("read:org");
-
-                    options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
-                    options.TokenEndpoint = "https://github.com/login/oauth/access_token";
-                    options.UserInformationEndpoint = "https://api.github.com/user";
-
-                    options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
-                    options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
-                    options.ClaimActions.MapJsonKey("urn:github:login", "login");
-                    options.ClaimActions.MapJsonKey("urn:github:url", "html_url");
-                    options.ClaimActions.MapJsonKey("urn:github:avatar", "avatar_url");
-                    options.ClaimActions.MapJsonKey("urn:github:orgs", "orgs_list");
-
-                    options.Events = new OAuthEvents
-                    {
-                        OnCreatingTicket = async context =>
-                        {
-                            var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
-                            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
-
-                            var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
-                            response.EnsureSuccessStatusCode();
-
-                            var user = JObject.Parse(await response.Content.ReadAsStringAsync());
-
-                            // Get organisation membership
-                            request = new HttpRequestMessage(HttpMethod.Get, $"{options.UserInformationEndpoint}/orgs");
-                            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
-
-                            response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
-                            response.EnsureSuccessStatusCode();
-
-                            var organisations = JArray.Parse(await response.Content.ReadAsStringAsync());
-
-                            var organisationNames = organisations.Select(x => x.Value<string>("login"));
-
-                            user.Add("orgs_list", String.Join(",", organisationNames));
-
-                            context.RunClaimActions(user);
-
-                        }
-                    };
-                });
-
+            services.AddGutHubAuthentication(Configuration);
             services.AddAuthorization(options =>
             {
                 options.AddPolicy("ValidOrgsOnly", policy => policy.Requirements.Add(new ValidOrganizationRequirement(Configuration["ValidOrganizations"])));
@@ -174,6 +89,7 @@ namespace SFA.DAS.SecureMessageService.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddApplicationInsights(app.ApplicationServices, LogLevel.Warning);
             app.UseAuthentication();
 
             if (env.IsDevelopment())
@@ -193,9 +109,6 @@ namespace SFA.DAS.SecureMessageService.Web
                 context.Response.Headers.Add("X-Xss-Protection", "1");
                 await next();
             });
-
-            // Enable app insights logging
-            loggerFactory.AddApplicationInsights(app.ApplicationServices, LogLevel.Warning);
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
