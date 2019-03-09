bc6aaa29ee3be8e1ff424f1faf9c7875f94db2be
Craig Gumbley <craiggumbley@gmail.com>
2019-03-09 19:27:01 +0000

    Remove message actions from home controller

--- src/SFA.DAS.SecureMessageService.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.SecureMessageService.Web/Controllers/HomeController.cs
@@ -18,39 +18,46 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace SFA.DAS.SecureMessageService.Web.Controllers
 {
-    [Authorize(Policy="ValidOrgsOnly")]
+    [Authorize(Policy = "ValidOrgsOnly")]
     public class HomeController : Controller
     {
         private readonly ILogger logger;
         private readonly IMessageService messageService;
-        
+
         public HomeController(ILogger<HomeController> _logger, IMessageService _messageService)
         {
             logger = _logger;
             messageService = _messageService;
         }
 
+        [AllowAnonymous]
         [HttpGet("")]
         public IActionResult Index()
         {
-            return View("Index", new IndexViewModel());
-        }
-
-        [HttpPost("")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> IndexSubmitMessage(IndexViewModel indexViewModel)
-        {
-            if (String.IsNullOrEmpty(indexViewModel.Message))
+            if (HttpContext.User.Identity.IsAuthenticated)
             {
-                logger.LogError(1, "Message cannot be null");
-                return new BadRequestResult();
+                logger.LogInformation(1, "User is authenticated, redirecting to Messages/CreateMessage()");
+                return RedirectToAction("CreateMessage", "Messages");
             }
 
-            var key = await messageService.Create(indexViewModel.Message, indexViewModel.Ttl);
-            logger.LogInformation(1, $"Saving message: {key}");
-
-            return RedirectToAction("ShareMessageUrl", "Messages", new { key = key });
+            return View("Index");
         }
+
+        // [HttpPost("")]
+        // [ValidateAntiForgeryToken]
+        // public async Task<IActionResult> IndexSubmitMessage(IndexViewModel indexViewModel)
+        // {
+        //     if (String.IsNullOrEmpty(indexViewModel.Message))
+        //     {
+        //         logger.LogError(1, "Message cannot be null");
+        //         return new BadRequestResult();
+        //     }
+
+        //     var key = await messageService.Create(indexViewModel.Message, indexViewModel.Ttl);
+        //     logger.LogInformation(1, $"Saving message: {key}");
+
+        //     return RedirectToAction("ShareMessageUrl", "Messages", new { key = key });
+        // }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
