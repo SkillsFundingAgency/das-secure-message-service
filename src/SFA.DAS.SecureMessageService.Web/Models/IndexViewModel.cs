ba3233deabe90fa14c100d9974fceace49ab6269
EwanNoble <ewan.noble@education.gov.uk>
2019-05-01 12:17:13 +0100

    Move viewmodel classes into namespace

--- src/SFA.DAS.SecureMessageService.Web/Models/IndexViewModel.cs
+++ src/SFA.DAS.SecureMessageService.Web/Models/IndexViewModel.cs
@@ -2,19 +2,20 @@ using System;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 
-public class IndexViewModel
+namespace SFA.DAS.SecureMessageService.Web.Models
 {
-    public IndexViewModel()
+    public class IndexViewModel
     {
-        TtlValues = new Dictionary<int, string>()
+        public IndexViewModel()
+        {
+            TtlValues = new Dictionary<int, string>()
         {
             { 1, "Hour" },
             { 24, "Day" }
         };
+        }
+        public string Message { get; set; }
+        public int Ttl { get; set; }
+        public Dictionary<int, string> TtlValues { get; set; }
     }
-    public string Message { get; set; }
-    public int Ttl {get;set;}
-    public Dictionary<int, string> TtlValues { get; set; }
-    
-    
 }
