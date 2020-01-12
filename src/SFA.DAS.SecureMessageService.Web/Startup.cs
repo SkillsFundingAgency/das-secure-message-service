42dae6aae171e96e0fd9655bd7569bcdf984650b
Craig Gumbley <craiggumbley@gmail.com>
2020-01-12 07:14:12 +0000

    use cookie.path

--- src/SFA.DAS.SecureMessageService.Web/Startup.cs
+++ src/SFA.DAS.SecureMessageService.Web/Startup.cs
@@ -48,16 +48,17 @@ namespace SFA.DAS.SecureMessageService.Web
             })
             .AddCookie(options =>
             {
+                options.LoginPath = new PathString("/Account/Login");
                 options.LogoutPath = new PathString("/Account/Logout");
-                // options.Cookie.Path = "/";
-                options.Events = new CookieAuthenticationEvents()
-                {
-                    OnRedirectToLogin = (context) =>
-                    {
-                        context.HttpContext.Response.Redirect($"https://{Configuration["BaseUrl"]}/Account/login");
-                        return Task.CompletedTask;
-                    }
-                };
+                options.Cookie.Path = "/";
+                // options.Events = new CookieAuthenticationEvents()
+                // {
+                //     OnRedirectToLogin = (context) =>
+                //     {
+                //         context.HttpContext.Response.Redirect($"https://{Configuration["BaseUrl"]}/Account/login");
+                //         return Task.CompletedTask;
+                //     }
+                // };
             });
 
             services.SetupSecureMessageService(Configuration, _env);
