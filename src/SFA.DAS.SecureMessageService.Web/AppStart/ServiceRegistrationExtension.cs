9fecd0196786551227bb76d6a6aa18c22f188e41
Craig Gumbley <craiggumbley@gmail.com>
2020-04-16 11:23:51 +0100

    Shared components (#37)
    * consume shared config
    
    * Add custom ai initializer
    
    * Remove arm template
    
    * Add custom initializer to api
    
    * consume baseurl from shared config
    
    * Update app insights package
    
    * Update app insights package

--- src/SFA.DAS.SecureMessageService.Web/AppStart/ServiceRegistrationExtension.cs
+++ src/SFA.DAS.SecureMessageService.Web/AppStart/ServiceRegistrationExtension.cs
@@ -1,9 +1,11 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ApplicationInsights.Extensibility;
 using SFA.DAS.SecureMessageService.Core.IRepositories;
 using SFA.DAS.SecureMessageService.Core.IServices;
 using SFA.DAS.SecureMessageService.Core.Services;
 using SFA.DAS.SecureMessageService.Infrastructure.Repositories;
+using SFA.DAS.SecureMessageService.Web.Infrastructure;
 
 namespace SFA.DAS.SecureMessageService.Web.AppStart
 {
@@ -17,6 +19,7 @@ namespace SFA.DAS.SecureMessageService.Web.AppStart
             services.AddSingleton<IDasDistributedCache, DasDistributedCache>();
             services.AddSingleton<IDasDataProtector, DasDataProtector>();
             services.AddSingleton<ISecureKeyRepository, SecureKeyRepository>();
+            services.AddSingleton<ITelemetryInitializer, ToolsTelemetryInitializer>();
         }
     }
 }
