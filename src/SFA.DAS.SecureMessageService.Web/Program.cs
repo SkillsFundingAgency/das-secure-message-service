61b44caa5f009641288fe22b6b56838c5951eaef
HussainsGitHub <85938019+HussainsGitHub@users.noreply.github.com>
2026-03-04 16:56:25 +0000

    DASD-14737 - Updated health check ports to 8080 and fixed anti forgery error

--- src/SFA.DAS.SecureMessageService.Web/Program.cs
+++ src/SFA.DAS.SecureMessageService.Web/Program.cs
@@ -87,12 +87,12 @@ app.Use(async (context, next) =>
         context.Response.Headers.Remove("X-Frame-Options");
 
     context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
-    
+
     await next();
 
     if (context.Response.StatusCode == 404 && !context.Response.HasStarted)
     {
-         //Re-execute the request so the user gets the error page
+        //Re-execute the request so the user gets the error page
         var originalPath = context.Request.Path.Value;
         context.Items["originalPath"] = originalPath;
         context.Request.Path = "/error/404";
